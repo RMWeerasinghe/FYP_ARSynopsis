61a11fc22c70ba5a479745b7e4e704fb7d5cf0c8
Amila29 <amilakasun2000@gmail.com>
2024-10-15 00:08:31 +0530

    Add function to remove email addresses from text

---
+++ Embeddings/preprocessing.py
@@ -0,0 +1,6 @@
+import re
+
+# Function to remove email addresses from text
+def remove_emails(text):
+    email_pattern = r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}'
+    return re.sub(email_pattern, '', text) 
